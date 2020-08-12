ba429394e613c3057d16f60f91510e070af7b072
kang hyeon <kh90177287@gmail.com>
2020-08-12 16:42:59 +0900

    아이콘 추가

---
+++ src/write_review.php
@@ -0,0 +1,25 @@
+<?php 
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '111111',
+  'web11'
+);
+$sql = "SELECT * FROM topic";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/topbar.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <script src="https://kit.fontawesome.com/78e43f918f.js" crossorigin="anonymous"></script>
+    <title>리뷰 작성</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
