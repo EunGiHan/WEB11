aa5261e9eb23aef650ea51ce0895b674ef1c3693
EunGiHan <69252153+EunGiHan@users.noreply.github.com>
2020-08-22 23:28:50 +0900

    Update check.php

--- src/check.php
+++ src/check.php
@@ -36,20 +36,12 @@
 
     $sql = "SELECT * FROM member WHERE userid={$_GET['userid']}";
 		$result = mysqli_query($conn, $sql);
-		//echo "{$result}";
-		//$row = mysqli_fetch_array($result);$row[0]==""
-		$num = mysql_num_rows($result);
-		if ($num > 0) {
-			echo "<div>{$_GET['userid']}는 중복된 아이디입니다.</div>";
-		} else {
+		if (mysqli_num_rows($result) == "0") {
 			echo "<div>{$_GET['userid']}는 사용 가능한 아이디입니다.</div>";
-		}
-	if($result==FALSE)	{
-		//echo "<div>{$_GET['userid']}는 사용 가능한 아이디입니다.</div>";
-	} else{
-		//echo "<div>{$_GET['userid']}는 중복된 아이디입니다.</div>";
+		} else {
 
-	}
+			echo "<div>{$_GET['userid']}는 중복된 아이디입니다.</div>";
+		}
 ?>
 <button value="닫기" onclick="window.close()">닫기</button>
 <script>
