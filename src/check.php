a08b12723661c1a8665afddd1d1eb762aaa0d503
EunGiHan <69252153+EunGiHan@users.noreply.github.com>
2020-08-22 18:11:52 +0900

    0822 update_review, check 수정중
    0822 update_review, check 수정중

--- src/check.php
+++ src/check.php
@@ -1,3 +1,30 @@
+<!--<meta charset="utf-8">
+<?/*php
+	$conn = mysqli_connect(
+        'localhost',
+        'inhapot',
+        'inha8302#11',
+        'inhapot'
+    );
+
+    $sql = "select * from member where id={$_GET['userid']}";
+	if(!$member = mysqli_query($conn, $sql))
+	{
+?>
+<div><?php echo $_GET['userid']; ?>는 사용가능한 아이디입니다.</div>
+<?php
+	}else{
+        ?>
+<div><?php echo $_GET['userid']; ?>는 중복된아이디입니다.<div>
+        <?php
+			}*/
+        ?>
+        <button value="닫기" onclick="window.close()">닫기</button>
+        <script>
+        </script>-->
+
+
+
 <meta charset="utf-8">
 <?php
 	$conn = mysqli_connect(
@@ -6,19 +33,24 @@
         'inha8302#11',
         'inhapot'
     );
-    
-    $sql = "select * from member where id={$_GET['userid']}";
-	if(!$member = mysqli_query($conn, $sql))
-	{
+
+    $sql = "SELECT * FROM member WHERE userid={$_GET['userid']}";
+		$result = mysqli_query($conn, $sql);
+		//echo "{$result}";
+		//$row = mysqli_fetch_array($result);$row[0]==""
+		$num = mysql_num_rows($result);
+		if ($num > 0) {
+			echo "<div>{$_GET['userid']}는 중복된 아이디입니다.</div>";
+		} else {
+			echo "<div>{$_GET['userid']}는 사용 가능한 아이디입니다.</div>";
+		}
+	if($result==FALSE)	{
+		//echo "<div>{$_GET['userid']}는 사용 가능한 아이디입니다.</div>";
+	} else{
+		//echo "<div>{$_GET['userid']}는 중복된 아이디입니다.</div>";
+
+	}
 ?>
-<div><?php echo $_GET['userid']; ?>는 사용가능한 아이디입니다.</div>
-<?php 
-	}else{
-        ?>
-<div><?php echo $_GET['userid']; ?>는 중복된아이디입니다.<div>
-        <?php
-            }
-        ?>
-        <button value="닫기" onclick="window.close()">닫기</button>
-        <script>
-        </script>
+<button value="닫기" onclick="window.close()">닫기</button>
+<script>
+</script>
