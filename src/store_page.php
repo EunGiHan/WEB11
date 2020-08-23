4ee24a2fca683a7aad98e6093ef97d353b23e770
kang hyeon <kh90177287@gmail.com>
2020-08-23 23:24:25 +0900

    continue update_review

--- src/store_page.php
+++ src/store_page.php
@@ -25,23 +25,23 @@ while($row = mysqli_fetch_array($result)){
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <script src="https://kit.fontawesome.com/78e43f918f.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../css/restaurant.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../css/responsive.css">
-  <title><?=$name?></title>
+    <meta charset="utf-8">
+    <script src="https://kit.fontawesome.com/78e43f918f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/restaurant.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <title><?=$name?></title>
 </head>
 
 <body>
-  <div class="container">
-      <div class="top">
-          <input type="button" class="btn" onclick="location.href='javascript:history.back()';" value="back">
-          <input type="button" class="btn1" onclick="location.href='../index.php';" value="home" >
-      </div>
-      <div class="title">
-          <b><?=$name?></b><br>
-          <?php
+    <div class="container">
+        <div class="top">
+            <input type="button" class="btn" onclick="location.href='javascript:history.back()';" value="back">
+            <input type="button" class="btn1" onclick="location.href='../index.php';" value="home">
+        </div>
+        <div class="title">
+            <b><?=$name?></b><br>
+            <?php
             for($i=5; $i>0; $i--){
               if($star > 0){
                 echo "<span class=\"fa fa-star checked\"></span>";
@@ -53,53 +53,55 @@ while($row = mysqli_fetch_array($result)){
             echo "($review_amount)";
 
            ?>
-      </div>
-      <table>
-          <td id="share">공유</td>
-          <td><a href="../src/write_review.php?store_id=<?=$_GET['store_id']?>">리뷰 작성하기</td>
-      </table>
-      <div class="information">
-          <strong>정보</strong>
-          <a href=#none id="show" onclick="if(hide1.style.display=='none') {hide1.style.display='';show.innerText='▲'} else {hide1.style.display='none';show.innerText='▶'}">▶</a>
-          <div id="hide1" style="display: none">
-            <?php
+        </div>
+        <table>
+            <td id="share">공유</td>
+            <td><a href="../src/write_review.php?store_id=<?=$_GET['store_id']?>">리뷰 작성하기</td>
+        </table>
+        <div class="information">
+            <strong>정보</strong>
+            <a href=#none id="show"
+                onclick="if(hide1.style.display=='none') {hide1.style.display='';show.innerText='▲'} else {hide1.style.display='none';show.innerText='▶'}">▶</a>
+            <div id="hide1" style="display: none">
+                <?php
               echo "<div class=\"text1\">주소<div class=\"address\">"."{$row['address']}"."</div></div>";
               echo "<div class=\"text2\">영업시간<div class=\"time\">"."{$row['hour']}"."</div></div>";
               echo "<div class=\"text3\">전화번호<div id=\"plus\">"."{$row['tel']}"."</div></div>";
             }
              ?>
-            <div style="text-align: center;"><a href="update_info.php?store_id=<?=$_GET['store_id']?>">정보 수정 제안하기</a></div>
-          </div>
-      </div>
-      <div class="menubox">
-          <strong>메뉴</strong><br>
-          <div class="menu">
-            <?php
+                <div style="text-align: center;"><a href="update_info.php?store_id=<?=$_GET['store_id']?>">정보 수정
+                        제안하기</a></div>
+            </div>
+        </div>
+        <div class="menubox">
+            <strong>메뉴</strong><br>
+            <div class="menu">
+                <?php
             $sql = "SELECT main_menu, main_menu_price FROM stores WHERE id={$_GET['store_id']}";
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_array($result);
             echo "[대표 메뉴]    "."{$row['main_menu']}"."  ---  "."{$row['main_menu_price']}"."원<br>";
              ?>
-          </div>
-          <div id="show2">
-            <a href=#none id="show2"
-              onclick="if(hide2.style.display=='none') {hide2.style.display='';show2.innerText='접기'} else {hide2.style.display='none';show2.innerText='다른 메뉴 더보기'}">
-              다른 메뉴 더보기
-            </a>
-          </div>
-          <div id="hide2" style="display: none">
-              <?php
+            </div>
+            <div id="show2">
+                <a href=#none id="show2"
+                    onclick="if(hide2.style.display=='none') {hide2.style.display='';show2.innerText='접기'} else {hide2.style.display='none';show2.innerText='다른 메뉴 더보기'}">
+                    다른 메뉴 더보기
+                </a>
+            </div>
+            <div id="hide2" style="display: none">
+                <?php
               $sql = "SELECT * FROM menus WHERE store_id={$_GET['store_id']} ORDER BY price DESC";
               $result = mysqli_query($conn, $sql);
               while($row = mysqli_fetch_array($result)){
                 echo "{$row['menu']}"."---"."{$row['price']}"."원<br>";
               }
               ?>
-              <a href="update_menu.php?store_id=<?=$_GET['store_id']?>">메뉴 정보 수정 제안하기</a>
-          </div>
-      </div>
-      <div class="reviewbox">
-      <?php
+                <a href="update_menu.php?store_id=<?=$_GET['store_id']?>">메뉴 정보 수정 제안하기</a>
+            </div>
+        </div>
+        <div class="reviewbox">
+            <?php
       echo "<strong>리뷰 ("."{$review_amount}"."건)</strong>";
       $sql = "SELECT * FROM reviews WHERE store_id={$_GET['store_id']}";
       $result = mysqli_query($conn, $sql);
@@ -109,7 +111,10 @@ while($row = mysqli_fetch_array($result)){
 
         $class_rating = "<div class=\"rating\">";
         echo $class_rating;
+
+          // author 표현
         echo "{$row['author']}"."<br>";
+        // 별 표현
         $star = $row['star'];
         for($i=5; $i>0; $i--){
           if($star > 0){
@@ -121,20 +126,24 @@ while($row = mysqli_fetch_array($result)){
         }
 
         echo "</div>";
+        // comment 표현
         echo "<div class=\"comment\">"."{$row['review']}"."</div></div>";
 
+          // 아이디 네임이랑 리뷰 네임이랑 같으면 업데이트 표시
         if($_SESSION['name'] == $row['author']){
           $update_link = '<a href="update_review.php?store_id='.$_GET['store_id'].'&id='.$row['id'].'">수정</a>';
           echo $update_link;
           echo '<form action="process_delete.php" method="post">
-                  <input type="hidden" name="store_id" value"'.$_GET['id'].'">
+                  <input type="hidden" name="store_id" value="{$_GET[\'id\']}">
+                  <input type="hidden" name="star_count" value="'.$row['star'].'">
+                  <input type="hidden" name="comment" value="'.$row['review'].'">
                   <input type="submit" value="삭제">
                 </form>';
         }
       }
        ?>
-     </div>
-  </div>
+        </div>
+    </div>
 </body>
 
 </html>
