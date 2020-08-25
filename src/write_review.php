80a212d8071d9988feb6fb59b30d4009e68e15ac
sare112 <69349374+sare112@users.noreply.github.com>
2020-08-26 00:06:11 +0900

    Update write_review.php

--- src/write_review.php
+++ src/write_review.php
@@ -26,17 +26,18 @@ while($row=mysqli_fetch_array($result)){
 <body>
     <div class="container">
         <div class="top">
-            <input type="button" class="btn" onclick="location.href='javascript:history.back()';" value="back">
-            <input type="button" class="btn1" onclick="location.href='index.html';" value="home" >
+            <div style="text-align:center;"><a href="index.html"><b style="color:white; text-decoration:none;">INHA-POT</b></a></div>
+            <input type="button" class="btn1" onclick="location.href='javascript:history.back()';" value="back">
         </div>
         <div><h1>식당이름</h1></div><br>
-        <div style="border-top: 1px solid gainsboro;">별점을 선택하세요.</div><br>
+        <div class="border"></div><br>
+        <div style="text-align:center;">별점을 선택하세요.</div><br>
         <div class="rating">
             <fieldset>   
                 <span class="star-cb-group">
                     <input type="radio" id="rating-5" name="rating" value="5" />
                     <label for="rating-5">5</label>
-                    <input type="radio" id="rating-4" name="rating" value="4" checked="checked" />
+                    <input type="radio" id="rating-4" name="rating" value="4" />
                     <label for="rating-4">4</label>
                     <input type="radio" id="rating-3" name="rating" value="3" />
                     <label for="rating-3">3</label>
