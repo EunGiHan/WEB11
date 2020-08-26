cf8fb089cf952a31f9d44876a7b84de0a6e1574e
sare112 <69349374+sare112@users.noreply.github.com>
2020-08-26 12:57:23 +0900

    Update member.php

--- src/member.php
+++ src/member.php
@@ -20,40 +20,43 @@
         }
         </script>
         <link rel="stylesheet" href="../css/member.css">
+        <link rel="stylesheet" href="../css/responsive.css">
     </head>
     <body>
-        <form action="./member_ok.php" method="POST">
-            <a href="index.html"><h1 style="font-size:50px;">INHA-POT</h1></a>
-            <h2>회원가입</h2>
-            <table>
-                <tr>
-                    <td>Name</td>
-                    <td><input type="text" name="name" placeholder="이름" style="width:280px;"></td>
-                </tr>
-                <tr>
-                    <td>ID</td>
-                    <td><input type="text" name="userid" placeholder="아이디" id="chk_id1" style="width:280px;"></td>
-                    <td><input type="button" value="중복검사" onclick="checkid();"></td>
-                    <td><input type="hidden" value="0" name="chs" id="chk_id2"></td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td><input type="password" name="userpw" placeholder="비밀번호" style="width:280px;"></td>
-                </tr>
-                <tr>
-                    <td>email</td>
-                    <td><input type="text" name="email" placeholder="이메일">@
-                        <select name="emailaddress">
-                            <option value="naver.com">naver.com</option>
-                            <option value="gmail.com">gmail.com</option>
-                            <option value="daum.net">daum.net</option>
-                            <option value="hanmail.com">hanmail.com</option>
-                        </select>
-                    </td>
-                </tr>
-            </table>
-            <button type="submit" id="btn">회원가입</button>
-        </form>
-        <iframe src="" id="ifrm1" scrolling=no frameborder=no width=0 height=0 name="ifrm1"></iframe>
+        <div class="container">
+            <form action="./member_ok.php" method="POST">
+                <a href="index.html"><h1 style="font-size:50px;">INHA-POT</h1></a>
+                <h2>회원가입</h2>
+                <table>
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" name="name" placeholder="이름" style="width:280px;"></td>
+                    </tr>
+                    <tr>
+                        <td>ID</td>
+                        <td><input type="text" name="userid" placeholder="아이디" id="chk_id1" style="width:280px;"></td>
+                        <td><input type="button" value="중복검사" onclick="checkid();"></td>
+                        <td><input type="hidden" value="0" name="chs" id="chk_id2"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="userpw" placeholder="비밀번호" style="width:280px;"></td>
+                    </tr>
+                    <tr>
+                        <td>email</td>
+                        <td><input type="text" name="email" placeholder="이메일">@
+                            <select name="emailaddress">
+                                <option value="naver.com">naver.com</option>
+                                <option value="gmail.com">gmail.com</option>
+                                <option value="daum.net">daum.net</option>
+                                <option value="hanmail.com">hanmail.com</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+                <button type="submit" id="btn">회원가입</button>
+            </form>
+            <iframe src="" id="ifrm1" scrolling=no frameborder=no width=0 height=0 name="ifrm1"></iframe>
+        </div>
     </body>
 </html>
